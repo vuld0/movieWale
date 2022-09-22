094daddc057f93fd5dd5bc2dc5667d1a2b3b1a25
Naveen <naveensreevalsan@gmail.com>
2022-09-22 12:41:58 +0530

    register ccomponent added

--- views/src/components/register.component.tsx
+++ views/src/components/register.component.tsx
@@ -1,1 +1,169 @@
 import { Component } from "react";
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import * as Yup from "yup";
+
+import AuthService from "../services/auth.service";
+
+type Props = {};
+
+type State = {
+    username: string,
+    email: string,
+    password: string,
+    successful: boolean,
+    message: string
+};
+
+export default class Register extends Component<Props,State>{
+    constructor(props:Props){
+        super(props);
+        this.state = {
+            username: "",
+            email: "",
+            password: "",
+            successful: false,
+            message: ""
+        };
+    }
+
+    validationSchema(){
+        return Yup.object().shape({
+            username: Yup.string()
+                .test(
+                    "len",
+                    "The username must be between 3 and 20 characters.",
+                    (val: any) =>
+                        val &&
+                        val.toString().length >= 6 &&
+                        val.toString().length <= 40
+                )
+                .required("This field is required!"),
+            
+            email: Yup.string()
+                .test(
+                    "len",
+                    "The username must be between 3 and 20 characters.",
+                    (val: any) =>
+                        val &&
+                        val.toString().length >= 6 &&
+                        val.toString().length <= 40
+                )
+                .required("This field is required!"),
+        });
+    }
+
+    handleRegister(formValue: { username: string; email: string; password: string}) {
+
+        const {username, email, password} = formValue;
+
+        this.setState({
+            message: "",
+            successful: false
+        });
+
+        AuthService.register(
+            username,
+            email,
+            password
+        ).then(
+            response => {
+                this.setState({
+                    message: response.data.message,
+                    successful: true
+                });
+            },
+            error => {
+                const resMessage =
+                    (error.response && error.response.data && error.response.data.message) ||
+                    error.messages || error.toString();
+
+                this.setState({
+                    successful: false,
+                    message: resMessage
+                });
+            }
+        )
+    }
+
+
+    render() {
+        const { successful, message } = this.state;
+
+        const initialValues = {
+            username: "",
+            email: "",
+            password: "",
+        };
+        return (
+            <div className="col-md-12">
+                <div className="card card-container">
+                    <img
+                        src="//ssl.gstatic.com/accounts/ui/avatar_2x.png"
+                        alt="profile-img"
+                        className="profile-img-card"
+                    />
+
+                    <Formik 
+                        initialValues={initialValues}
+                        validationSchema={this.validationSchema}
+                        onSubmit={this.handleRegister}
+                    >
+                        <Form>
+                            {!successful && (
+                                <div>
+                                    <div className="form-group">
+                                        <label htmlFor="username"> Username </label>
+                                        <Field name="username" type="text" className="form-control" />
+                                        <ErrorMessage
+                                            name="username"
+                                            component="div"
+                                            className="alert alert-danger"
+                                         />
+                                    </div>
+
+                                    <div className="form-group">
+                                        <label htmlFor="email"> Email </label>
+                                        <Field name="email" type="email" className="form-control" />
+                                        <ErrorMessage
+                                            name="email"
+                                            component="div"
+                                            className="alert alert-danger"
+                                         />
+                                    </div>
+
+                                    <div className="form-group">
+                                        <label htmlFor="password"> Password </label>
+                                        <Field name="password" type="password" className="form-control" />
+                                        <ErrorMessage
+                                            name="password"
+                                            component="div"
+                                            className="alert alert-danger"
+                                         />
+                                    </div>
+
+                                    <div className="form-group">
+                                        <button type="submit" className="bbtn btn-primary btn-block">Sign Up</button>
+                                    </div>
+                                </div>
+                            )}
+
+                            {message && (
+                                <div className="form-group">
+                                    <div className={
+                                            successful ? "alert alert-success" : "alert alert-danger"
+                                        }
+                                        role="alert"
+                                    >
+                                        {message}
+                                    </div>
+                                </div>
+                            )}
+                        </Form>
+
+                    </Formik>
+                </div>
+
+            </div>
+        );
+    }
+}
